35f4384a77553ab92cd7e56bf4d1fb67a9a22562
Zarck <zarck430@gmail.com>
2026-08-05 06:11:34 -0600

    Delete InventaMeCF directory

--- InventaMeCF/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace InventaMeCF.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- InventaMeCF/Models/InfoLogin.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace InventaMeCF.Models
-{
-    public class InfoLogin
-    {
-        public string? Login { get; set; }
-        public string? Password { get; set; }
-    }
-}
